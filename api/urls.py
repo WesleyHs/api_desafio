45bed2c2b36fdb4372a727df445aa857ea0bca8b
WesleyHs <wesley.henrique.pgm@gmail.com>
2025-04-22 22:06:37 -0300

    funcao order e date

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('upload/', views.uploadView.as_view(), name='upload'),
     path('all/', views.allView.as_view(), name='all'),
+    path('order/<int:order_id>/', views.orderView.as_view(), name='order'),
+    path('date/', views.dateView.as_view(), name='date'),
 ]
